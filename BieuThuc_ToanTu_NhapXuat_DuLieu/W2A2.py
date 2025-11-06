7a17bacfc9de84a9625868504c94925292e83d7d
ThanhPhong2307 <25021572@uet.edu.vn>
2025-11-06 15:43:03 +0700

    hello

---
+++ BieuThuc_ToanTu_NhapXuat_DuLieu/W2A2.py
@@ -0,0 +1,2 @@
+ten_nguoi_dung = input("Mời bạn nhập tên của mình: ")
+print("Chào " + ten_nguoi_dung + "!")
---
+++ BieuThuc_ToanTu_NhapXuat_DuLieu/W2A3.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+tong = a + b
+hieu = a - b
+tich = a * b
+phan_nguyen = a // b
+phan_du = a % b
+chia_thuc = a / b
+print(f"Tong: {tong}")
+print(f"Hieu: {hieu}")
+print(f"Tich: {tich}")
+print(f"Phan nguyen: {phan_nguyen}")
+print(f"Phan du: {phan_du}")
+print(f"Ket qua chia thuc: {chia_thuc}")
