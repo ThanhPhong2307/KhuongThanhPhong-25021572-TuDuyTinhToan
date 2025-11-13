d4230162033ca963ab7af524a85b3faec89b2b0a
ThanhPhong2307 <25021572@uet.edu.vn>
2025-11-13 21:46:16 +0700

    Vui

--- BieuThuc_ToanTu_NhapXuat_DuLieu/W2A2.py
+++
@@ -1,2 +0,0 @@
-ten_nguoi_dung = input("Mời bạn nhập tên của mình: ")
-print("Chào " + ten_nguoi_dung + "!")
---
+++ chess/a/e.py
@@ -0,0 +1,4 @@
+import chess
+board = chess.Board()
+board.push_san("e2e4")
+print(board)
